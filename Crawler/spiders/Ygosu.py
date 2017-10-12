236619fa2cbd850bad4189ebd2b9c0b9913f4611
CarpDm <skdkfk8758@gmail.com>
2017-10-12 09:42:53 +0900

    크롤러 코드정리
    17.10.11
    this is game 오류수정

--- Crawler/spiders/Ygosu.py
+++ Crawler/spiders/Ygosu.py
@@ -21,49 +21,49 @@ class Ygosu(scrapy.Spider):
 
     # 각 게시판별로 리퀘스트 요청
     def start_requests(self):
-        for i in range(1, 2, 1):
+        for i in range(1, MAX_PAGE, 1):
             yield scrapy.Request("http://www.ygosu.com/community/yeobgi/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/free/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/issue/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/love/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/study/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/secret/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/horror/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/tip/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/jobs/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/soccer/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/baseball/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/basketball/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/sports/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/computer/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/phone/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/bicycle/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/animation/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/stars/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/movie/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/music/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/food/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/mil/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/fashion/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/hiphop/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/animal/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/stock/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/art/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/travel/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/game_etc/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/lo_legend/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/st/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/star2_st/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/over/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/fm/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/ffonline2/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/baram/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/black/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/hearthstone/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/diablo/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/df/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/maple/?page={}".format(i))
-            # yield scrapy.Request("http://www.ygosu.com/community/pubg/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/free/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/issue/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/love/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/study/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/secret/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/horror/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/tip/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/jobs/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/soccer/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/baseball/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/basketball/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/sports/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/computer/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/phone/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/bicycle/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/animation/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/stars/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/movie/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/music/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/food/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/mil/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/fashion/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/hiphop/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/animal/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/stock/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/art/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/travel/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/game_etc/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/lo_legend/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/st/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/star2_st/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/over/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/fm/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/ffonline2/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/baram/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/black/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/hearthstone/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/diablo/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/df/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/maple/?page={}".format(i))
+            yield scrapy.Request("http://www.ygosu.com/community/pubg/?page={}".format(i))
 
     # 리스폰스 받아서 사이트 파싱
     def parse(self, response):
@@ -83,7 +83,7 @@ class Ygosu(scrapy.Spider):
                 # 게시물 링크 저장
                 linkXpath = "td[@class='tit']/a/@href"
                 item['link'] = self.baseUrl + createItemUseXpath(select, linkXpath, texttype="link")
-                print(item['link'])
+                # print(item['link'])
 
                 # 현재 게시판 url을 분석해서 게시판 속성 저장
                 item['attribute'] = str(response).split('/')[4]
@@ -93,13 +93,13 @@ class Ygosu(scrapy.Spider):
                 tagName = "div"
                 tagAttr = {"class": "date"}
                 item['date'] = " ".join(createItemUseBs4(item['link'], tagName, tagAttr, texttype="date", encoding="utf8").split(" ")[0:2])
-                print(item['date'])
+                # print(item['date'])
 
                 # 게시물 조회수 저장
                 tagName = "div"
                 tagAttrs = {"class": "date"}
                 item['hits'] = createItemUseBs4(item['link'], tagName, tagAttrs, texttype="date", encoding="utf8").split(" ")[-1]
-                print(item['hits'])
+                # print(item['hits'])
 
                 # 추천수 저장
                 recommenedXpath = "td[@class='vote']/text()"
