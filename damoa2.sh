fa5b962e0119966b6002981aea5d01eff0a98f69
CarpDm <skdkfk8758@gmail.com>
2017-10-20 10:11:25 +0900

    2017.10.20
    BUG FIX

--- damoa2.sh
+++ damoa2.sh
@@ -1,17 +1,3 @@
-#! bin/bash
-
-scrapyd &
-
-python3.6 scrapyd-client/scrapyd_client/deploy.py
-
-curl http://localhost:6800/schedule.json -d project=Damoa -d spider=clien
-curl http://localhost:6800/schedule.json -d project=Damoa -d spider=bobaedream
-curl http://localhost:6800/schedule.json -d project=Damoa -d spider=gameshot
-curl http://localhost:6800/schedule.json -d project=Damoa -d spider=ruli
-curl http://localhost:6800/schedule.json -d project=Damoa -d spider=humoruniv
-curl http://localhost:6800/schedule.json -d project=Damoa -d spider=thisisgame
-curl http://localhost:6800/schedule.json -d project=Damoa -d spider=ygosu
-
 curl http://localhost:6800/schedule.json -d project=Damoa -d spider=becle
 curl http://localhost:6800/schedule.json -d project=Damoa -d spider=82cook
 curl http://localhost:6800/schedule.json -d project=Damoa -d spider=dramameeting
@@ -23,4 +9,3 @@ curl http://localhost:6800/schedule.json -d project=Damoa -d spider=underkg
 #curl http://localhost:6800/schedule.json -d project=Damoa -d spider=clien
 #curl http://localhost:6800/schedule.json -d project=Damoa -d spider=clien
 #curl http://localhost:6800/schedule.json -d project=Damoa -d spider=clien
-
