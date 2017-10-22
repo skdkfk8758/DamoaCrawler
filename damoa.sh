19623c48c077f930b0dfdd522c2a3e2cfba43696
CarpDm <skdkfk8758@gmail.com>
2017-10-22 10:58:02 +0900

    2017.10.21
    크롤러 추가 : giggle, 쿼사르존, 하드웨어배틀

---
+++ damoa.sh
@@ -0,0 +1,24 @@
+#! bin/bash
+
+scrapyd &
+
+python3.6 scrapyd-client/scrapyd_client/deploy.py
+
+curl http://localhost:6800/schedule.json -d project=Damoa -d spider=clien
+curl http://localhost:6800/schedule.json -d project=Damoa -d spider=bobaedream
+curl http://localhost:6800/schedule.json -d project=Damoa -d spider=gameshot
+curl http://localhost:6800/schedule.json -d project=Damoa -d spider=ruli
+curl http://localhost:6800/schedule.json -d project=Damoa -d spider=humoruniv
+curl http://localhost:6800/schedule.json -d project=Damoa -d spider=thisisgame
+curl http://localhost:6800/schedule.json -d project=Damoa -d spider=ygosu
+
+sleep 10
+
+curl http://localhost:6800/schedule.json -d project=Damoa -d spider=becle
+curl http://localhost:6800/schedule.json -d project=Damoa -d spider=82cook
+curl http://localhost:6800/schedule.json -d project=Damoa -d spider=dramameeting
+curl http://localhost:6800/schedule.json -d project=Damoa -d spider=underkg
+curl http://localhost:6800/schedule.json -d project=Damoa -d spider=giggle
+curl http://localhost:6800/schedule.json -d project=Damoa -d spider=hwbattle
+curl http://localhost:6800/schedule.json -d project=Damoa -d spider=quasarzone
+
